978be3351b4cd07d65310f729fee8d3a51e83211
Johannes Chorzempa <110151013+jmchor@users.noreply.github.com>
2023-02-23 00:53:37 +0100

    Setup project, tsup vs tsc

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,5 @@
-const headlineSpace = document.getElementById('headline-space') as HTMLDivElement;
-
+"use strict";
+const headlineSpace = document.getElementById('headline-space');
 const headline = document.createElement('h1');
 headline.textContent = 'Hello World!';
 headlineSpace.appendChild(headline);
